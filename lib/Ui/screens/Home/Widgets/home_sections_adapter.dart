5dfdc82578bb44f287cf18f61139691b11540c74
Mohammed El Shora <riyadm2001@gmail.com>
2024-08-08 17:06:11 +0300

    change item card ui

--- lib/Ui/screens/Home/Widgets/home_sections_adapter.dart
+++ lib/Ui/screens/Home/Widgets/home_sections_adapter.dart
@@ -2,15 +2,10 @@ import 'package:eClassify/Ui/screens/Home/home_screen.dart';
 import 'package:eClassify/Utils/Extensions/extensions.dart';
 import 'package:eClassify/Utils/responsiveSize.dart';
 import 'package:eClassify/Utils/ui_utils.dart';
-import 'package:eClassify/data/Repositories/favourites_repository.dart';
-import 'package:eClassify/data/cubits/favorite/manageFavCubit.dart';
 import 'package:eClassify/data/model/item/item_model.dart';
 import 'package:eClassify/exports/main_export.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../Utils/AppIcon.dart';
-
-import '../../../../data/cubits/favorite/favoriteCubit.dart';
 import '../../../../data/model/Home/home_screen_section.dart';
 
 import '../../Widgets/promoted_widget.dart';
@@ -52,6 +47,7 @@ class HomeSectionsAdapter extends StatelessWidget {
                     ItemModel? item = section.sectionData?[index];
 
                     return ItemCard(
+                      width: MediaQuery.sizeOf(context).width / 2.rw(context),
                       item: item,
                       bigCard: true,
                     );
@@ -87,7 +83,7 @@ class HomeSectionsAdapter extends StatelessWidget {
 
                     return ItemCard(
                       item: item,
-                      width: 144,
+                      width: MediaQuery.sizeOf(context).width / 2.3.rw(context),
                     );
                   },
                   total: section.sectionData?.length ?? 0,
@@ -247,27 +243,26 @@ class _ItemCardState extends State<ItemCard> {
       child: Container(
         width: widget.width ?? 250,
         decoration: BoxDecoration(
-          border: Border.all(
-            color: context.color.borderColor.darken(30),
-            width: 1,
-          ),
-          color: context.color.secondaryColor,
+          color: context.color.borderColor.darken(30),
           borderRadius: BorderRadius.circular(18),
         ),
         child: Stack(
           children: [
             Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Stack(
                   children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(18),
-                      child: UiUtils.getImage(
-                        widget.item?.image ?? "",
-                        height: imageHeight,
-                        width: double.infinity,
-                        fit: BoxFit.fill,
+                    Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: UiUtils.getImage(
+                          widget.item?.image ?? "",
+                          height: imageHeight,
+                          width: imageHeight,
+                          fit: BoxFit.fill,
+                        ),
                       ),
                     ),
                     if (widget.item?.isFeature ?? false)
@@ -279,140 +274,94 @@ class _ItemCardState extends State<ItemCard> {
                 ),
                 Expanded(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 10.0, vertical: 7.5),
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text("${Constant.currencySymbol} ${widget.item?.price?.toString()}")
-                            .bold()
-                            .color(context.color.territoryColor)
-                            .size(context.font.large),
-                        Text(widget.item!.name!)
-                            .firstUpperCaseWidget()
-                            .setMaxLines(lines: 2)
-                            .size(context.font.large),
-                        if (widget.item?.address != "")
-                          Row(
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 8, vertical: 4),
+                          decoration: BoxDecoration(
+                            color: context.color.territoryColor,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Text("${widget.item?.user?.name}")
+                              .bold()
+                              .color(Colors.white)
+                              .size(context.font.small),
+                        ),
+                        FittedBox(
+                          fit: BoxFit.scaleDown,
+                          child: Text(widget.item!.name!)
+                              .firstUpperCaseWidget()
+                              .bold()
+                              .setMaxLines(lines: 2)
+                              .size(context.font.large),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 12),
+                          child: Row(
                             children: [
-                              UiUtils.getSvg(
-                                AppIcons.location,
-                                width: widget.bigCard == true ? 10 : 8,
-                                height: widget.bigCard == true ? 13 : 11,
+                              Expanded(
+                                child: Row(
+                                  children: [
+                                    Icon(
+                                      Icons.visibility_outlined,
+                                      size: widget.bigCard == true ? 13 : 11,
+                                      color: context.color.textDefaultColor
+                                          .withOpacity(0.5),
+                                    ),
+                                    const SizedBox(width: 4),
+                                    Expanded(
+                                      child: Text(
+                                              "${widget.item?.views} ${"view".translate(context)}")
+                                          .size((widget.bigCard == true)
+                                              ? context.font.small
+                                              : context.font.smaller)
+                                          .color(context.color.textDefaultColor
+                                              .withOpacity(0.5))
+                                          .setMaxLines(lines: 2),
+                                    )
+                                  ],
+                                ),
                               ),
                               Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsetsDirectional.only(
-                                      start: 3.0),
-                                  child: Text(widget.item?.address ?? "")
-                                      .size((widget.bigCard == true)
-                                          ? context.font.small
-                                          : context.font.smaller)
-                                      .setMaxLines(lines: 1)
-                                      .color(context.color.textDefaultColor
-                                          .withOpacity(0.5)),
+                                child: Row(
+                                  children: [
+                                    Icon(
+                                      Icons.calendar_today_outlined,
+                                      size: widget.bigCard == true ? 13 : 11,
+                                      color: context.color.textDefaultColor
+                                          .withOpacity(0.5),
+                                    ),
+                                    const SizedBox(width: 4),
+                                    Expanded(
+                                      child: Text(
+                                              "${"daysRemining".translate(context)} ${widget.item?.views} ${"days".translate(context)}")
+                                          .size((widget.bigCard == true)
+                                              ? context.font.small
+                                              : context.font.smaller)
+                                          .color(context.color.textDefaultColor
+                                              .withOpacity(0.5))
+                                          .setMaxLines(lines: 2),
+                                    )
+                                  ],
                                 ),
-                              )
+                              ),
                             ],
                           ),
+                        ),
                       ],
                     ),
                   ),
                 ),
               ],
             ),
-            favButton(),
+            // favButton(),
           ],
         ),
       ),
     );
-  }
-
-  Widget favButton() {
-    bool isLike =
-        context.read<FavoriteCubit>().isItemFavorite(widget.item!.id!);
-
-    return BlocProvider(
-        create: (context) => UpdateFavoriteCubit(FavoriteRepository()),
-        child: BlocConsumer<FavoriteCubit, FavoriteState>(
-            bloc: context.read<FavoriteCubit>(),
-            listener: ((context, state) {
-              if (state is FavoriteFetchSuccess) {
-                isLike = context
-                    .read<FavoriteCubit>()
-                    .isItemFavorite(widget.item!.id!);
-                if (isLike) {
-                } else {}
-              }
-            }),
-            builder: (context, likeAndDislikeState) {
-              return BlocConsumer<UpdateFavoriteCubit, UpdateFavoriteState>(
-                  bloc: context.read<UpdateFavoriteCubit>(),
-                  listener: ((context, state) {
-                    if (state is UpdateFavoriteSuccess) {
-                      if (state.wasProcess) {
-                        context
-                            .read<FavoriteCubit>()
-                            .addFavoriteitem(state.item);
-                      } else {
-                        context
-                            .read<FavoriteCubit>()
-                            .removeFavoriteItem(state.item);
-                      }
-                    }
-                  }),
-                  builder: (context, state) {
-                    return PositionedDirectional(
-                      top: imageHeight - (likeButtonSize / 2) - 2,
-                      end: 16,
-                      child: InkWell(
-                        onTap: () {
-                          UiUtils.checkUser(
-                              onNotGuest: () {
-                                context
-                                    .read<UpdateFavoriteCubit>()
-                                    .setFavoriteItem(
-                                      item: widget.item!,
-                                      type: isLike ? 0 : 1,
-                                    );
-                              },
-                              context: context);
-                        },
-                        child: Container(
-                          width: likeButtonSize,
-                          height: likeButtonSize,
-                          decoration: BoxDecoration(
-                            color: context.color.secondaryColor,
-                            shape: BoxShape.circle,
-                            boxShadow:
-                                context.watch<AppThemeCubit>().state.appTheme ==
-                                        AppTheme.dark
-                                    ? null
-                                    : [
-                                        BoxShadow(
-                                          color: Colors.grey[300]!,
-                                          offset: const Offset(0, 2),
-                                          spreadRadius: 2,
-                                          blurRadius: 4,
-                                        ),
-                                      ],
-                          ),
-                          child: FittedBox(
-                            fit: BoxFit.none,
-                            child: state is UpdateFavoriteInProgress
-                                ? Center(child: UiUtils.progress())
-                                : UiUtils.getSvg(
-                                    isLike ? AppIcons.like_fill : AppIcons.like,
-                                    width: 22,
-                                    height: 22,
-                                    color: context.color.territoryColor,
-                                  ),
-                          ),
-                        ),
-                      ),
-                    );
-                  });
-            }));
   }
 }
