f1a55ae91c5c8a59d9052582304259d001438218
Mohammed El Shora <riyadm2001@gmail.com>
2024-08-29 12:38:37 +0300

    fix validation and hint in custom drop down

--- lib/Ui/screens/Item/add_item_screen/CustomFiledStructure/fields/custom_dropdown_field.dart
+++ lib/Ui/screens/Item/add_item_screen/CustomFiledStructure/fields/custom_dropdown_field.dart
@@ -1,5 +1,6 @@
 import 'package:eClassify/Utils/Extensions/extensions.dart';
 import 'package:eClassify/Utils/responsiveSize.dart';
+import 'package:eClassify/Utils/validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -29,7 +30,7 @@ class CustomFieldDropdown extends CustomField {
 
       selected = ""; // Ensure selected is null initially
       // Ensure blank option is included in the values
-     /* if (!(parameters['values'] as List).contains("")) {
+      /* if (!(parameters['values'] as List).contains("")) {
         (parameters['values'] as List).insert(0, "");
       }*/
     }
@@ -40,107 +41,127 @@ class CustomFieldDropdown extends CustomField {
 
   @override
   Widget render() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          children: [
-            Container(
-              width: 32.rw(context),
-              height: 32.rh(context),
-              decoration: BoxDecoration(
-                color: context.color.territoryColor.withOpacity(
-                  0.1,
-                ),
-                borderRadius: BorderRadius.circular(
-                  10,
-                ),
-              ),
-              child: SizedBox(
-                height: 20,
-                width: 20,
-                child: FittedBox(
-                  fit: BoxFit.none,
-                  child: UiUtils.imageType(parameters['image'],
-                      width: 20,
+    return CustomValidator<String>(
+        initialValue: selected,
+        validator: (String? value) {
+          if (parameters['required'] != 1) {
+            return null;
+          }
+
+          if (value?.isNotEmpty == true) {
+            return null;
+          }
+
+          return "pleaseSelectValue".translate(context);
+        },
+        builder: (state) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 32.rw(context),
+                    height: 32.rh(context),
+                    decoration: BoxDecoration(
+                      color: context.color.territoryColor.withOpacity(
+                        0.1,
+                      ),
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                    ),
+                    child: SizedBox(
                       height: 20,
-                      fit: BoxFit.cover,
-                      color: context.color.textDefaultColor),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 10.rw(
-                context,
-              ),
-            ),
-            Text(
-              parameters['name'],
-            )
-                .size(
-                  context.font.large,
-                )
-                .bold(weight: FontWeight.w500)
-                .color(
-                  context.color.textColorDark,
-                )
-          ],
-        ),
-        SizedBox(
-          height: 14.rh(context),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 0,
-          ),
-          child: Container(
-            decoration: BoxDecoration(
-                color: context.color.secondaryColor,
-                borderRadius: BorderRadius.circular(
-                  10,
-                ),
-                border: Border.all(
-                  width: 1,
-                  color: context.color.borderColor.darken(30),
-                )),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                width: double.infinity,
-                child: DropdownButton(
-                  value: selected?.isEmpty == true ? null : selected,
-                  dropdownColor: context.color.secondaryColor,
-                  isExpanded: true,
-                  padding: const EdgeInsets.symmetric(vertical: 5),
-                  icon: SvgPicture.asset(AppIcons.downArrow),
-                  isDense: true,
-                  borderRadius: BorderRadius.circular(10),
-                  style: TextStyle(
-                    color: context.color.textLightColor,
-                    fontSize: context.font.large,
+                      width: 20,
+                      child: FittedBox(
+                        fit: BoxFit.none,
+                        child: UiUtils.imageType(parameters['image'],
+                            width: 20,
+                            height: 20,
+                            fit: BoxFit.cover,
+                            color: context.color.textDefaultColor),
+                      ),
+                    ),
                   ),
-                  underline: const SizedBox.shrink(),
-                  items: (parameters['values'] as List<dynamic>)
-                      .map<DropdownMenuItem<dynamic>>((dynamic e) {
-                    return DropdownMenuItem<dynamic>(
-                      value: e,
-                      child: Text(e),
-                    );
-                  }).toList(),
-                  onChanged: (v) {
-                    selected = v.toString();
-                    update(() {});
-                    print("seleted dropdown****$selected");
-                    AbstractField.fieldsData.addAll({
-                      parameters['id'].toString(): [selected],
-                    });
-                  },
+                  SizedBox(width: 10.rw(context)),
+                  Text(parameters['name'])
+                      .size(context.font.large)
+                      .bold(weight: FontWeight.w500)
+                      .color(context.color.textColorDark)
+                ],
+              ),
+              SizedBox(height: 14.rh(context)),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: context.color.secondaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      width: 1,
+                      color: state.hasError
+                          ? context.color.error
+                          : context.color.borderColor.darken(30),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: DropdownButton(
+                        value: selected?.isEmpty == true ? null : selected,
+                        dropdownColor: context.color.secondaryColor,
+                        isExpanded: true,
+                        hint: parameters['name'] == null
+                            ? null
+                            : Text(parameters['name']),
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        icon: SvgPicture.asset(AppIcons.downArrow),
+                        isDense: true,
+                        borderRadius: BorderRadius.circular(10),
+                        style: TextStyle(
+                          color: context.color.textLightColor,
+                          fontSize: context.font.large,
+                        ),
+                        underline: const SizedBox.shrink(),
+                        items: (parameters['values'] as List<dynamic>)
+                            .map<DropdownMenuItem<dynamic>>((dynamic e) {
+                          return DropdownMenuItem<dynamic>(
+                            value: e,
+                            child: Text(e),
+                          );
+                        }).toList(),
+                        onChanged: (v) {
+                          selected = v.toString();
+                          state.didChange(selected);
+                          update(() {});
+                          AbstractField.fieldsData.addAll({
+                            parameters['id'].toString(): [selected],
+                          });
+                        },
+                      ),
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
-        ),
-      ],
-    );
+              Visibility(
+                visible: state.hasError,
+                child: Container(
+                  width: double.infinity,
+                  padding: const EdgeInsetsDirectional.only(start: 14.0),
+                  child: Text(
+                    state.errorText ?? '',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      fontSize: context.font.small,
+                      color: context.color.error,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          );
+        });
   }
 }
