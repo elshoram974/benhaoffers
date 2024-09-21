0afa7e7ef2fc3292dfb77b69ce491821fd9a8d61
Mohammed El Shora <riyadm2001@gmail.com>
2024-09-21 16:27:27 +0300

    fix custom drop down field

--- lib/Ui/screens/Item/add_item_screen/CustomFiledStructure/fields/custom_dropdown_field.dart
+++ lib/Ui/screens/Item/add_item_screen/CustomFiledStructure/fields/custom_dropdown_field.dart
@@ -2,9 +2,7 @@ import 'package:eClassify/Utils/Extensions/extensions.dart';
 import 'package:eClassify/Utils/responsiveSize.dart';
 import 'package:eClassify/Utils/validator.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
-import '../../../../../../Utils/AppIcon.dart';
 import '../../../../../../Utils/ui_utils.dart';
 import '../../../../Widgets/DynamicField/dynamic_field.dart';
 import '../custom_field.dart';
@@ -79,58 +77,130 @@ class CustomFieldDropdown extends CustomField {
                 ],
               ),
               SizedBox(height: 14.rh(context)),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: context.color.secondaryColor,
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(
-                      width: 1,
-                      color: state.hasError
-                          ? context.color.error
-                          : context.color.borderColor.darken(30),
-                    ),
+              SizedBox(
+                width: double.infinity,
+                child: PopupMenuButton<String>(
+                  position: PopupMenuPosition.under,
+                  color: const Color(0xFFEFEFEF),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: DropdownButton(
-                        value: selected?.isEmpty == true ? null : selected,
-                        dropdownColor: context.color.secondaryColor,
-                        isExpanded: true,
-                        hint: parameters['name'] == null
-                            ? null
-                            : Text(parameters['name']),
-                        padding: const EdgeInsets.symmetric(vertical: 5),
-                        icon: SvgPicture.asset(AppIcons.downArrow),
-                        isDense: true,
-                        borderRadius: BorderRadius.circular(10),
-                        style: TextStyle(
-                          color: context.color.textLightColor,
-                          fontSize: context.font.large,
+                  constraints: const BoxConstraints(
+                    maxHeight: 170,
+                    maxWidth: 185,
+                    minWidth: 185,
+                  ),
+                  onSelected: (value) {
+                    selected = value.toString();
+                    state.didChange(selected);
+                    update(() {});
+                    AbstractField.fieldsData.addAll({
+                      parameters['id'].toString(): [selected],
+                    });
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(16),
+                    clipBehavior: Clip.antiAlias,
+                    decoration: ShapeDecoration(
+                      color: context.color.secondaryColor,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                          width: 1,
+                          strokeAlign: BorderSide.strokeAlignCenter,
+                          color: state.hasError
+                              ? context.color.error
+                              : context.color.borderColor.darken(10),
                         ),
-                        underline: const SizedBox.shrink(),
-                        items: (parameters['values'] as List<dynamic>)
-                            .map<DropdownMenuItem<dynamic>>((dynamic e) {
-                          return DropdownMenuItem<dynamic>(
-                            value: e,
-                            child: Text(e),
-                          );
-                        }).toList(),
-                        onChanged: (v) {
-                          selected = v.toString();
-                          state.didChange(selected);
-                          update(() {});
-                          AbstractField.fieldsData.addAll({
-                            parameters['id'].toString(): [selected],
-                          });
-                        },
+                        borderRadius: BorderRadius.circular(8),
                       ),
                     ),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            selected?.isNotEmpty == true
+                                ? selected
+                                : parameters['name'],
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: context.font.large,
+                              color: context.color.textDefaultColor
+                                  .withOpacity(0.7),
+                            ),
+                          ),
+                        ),
+                        Icon(
+                          Icons.keyboard_arrow_down_outlined,
+                          color:
+                              context.color.textDefaultColor.withOpacity(0.5),
+                        )
+                      ],
+                    ),
                   ),
+                  itemBuilder: (context) {
+                    return (parameters['values'] as List<dynamic>)
+                        .map<PopupMenuEntry<String>>(
+                      (dynamic value) {
+                        return PopupMenuItem<String>(
+                          value: value,
+                          child: Container(
+                            padding: const EdgeInsets.all(6),
+                            alignment: AlignmentDirectional.centerStart,
+                            decoration: BoxDecoration(
+                              color: value == selected && selected != null
+                                  ? context.color.territoryColor
+                                  : context.color.secondaryColor,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Text(
+                              value,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w400,
+                                color: value == selected && selected != null
+                                    ? Colors.white
+                                    : Colors.black,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ).toList();
+                  },
                 ),
+                //  DropdownButton(
+                //   value: selected?.isEmpty == true ? null : selected,
+                //   dropdownColor: context.color.secondaryColor,
+                //   isExpanded: true,
+                //   hint: parameters['name'] == null
+                //       ? null
+                //       : Text(parameters['name']),
+                //   padding: const EdgeInsets.symmetric(vertical: 5),
+                //   icon: SvgPicture.asset(AppIcons.downArrow),
+                //   isDense: true,
+                //   borderRadius: BorderRadius.circular(10),
+                //   style: TextStyle(
+                //     color: context.color.textLightColor,
+                //     fontSize: context.font.large,
+                //   ),
+                //   underline: const SizedBox.shrink(),
+                //   items: (parameters['values'] as List<dynamic>)
+                //       .map<DropdownMenuItem<dynamic>>((dynamic e) {
+                //     return DropdownMenuItem<dynamic>(
+                //       value: e,
+                //       child: Text(e),
+                //     );
+                //   }).toList(),
+                //   onChanged: (v) {
+                //     selected = v.toString();
+                //     state.didChange(selected);
+                //     update(() {});
+                //     AbstractField.fieldsData.addAll({
+                //       parameters['id'].toString(): [selected],
+                //     });
+                //   },
+                // ),
               ),
               Visibility(
                 visible: state.hasError,
